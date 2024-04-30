a8cfe66bdf86b56f57c0b6262e5cf4c6b7e3712b
isabellaes <borjessonisabella@gmail.com>
2024-04-30 10:42:53 +0200

    set up base routing

---
+++ app/favourite/authors/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Fav authors</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      
+      <h1>Start Page</h1>
     </main>
   );
 }
