a3bda63c1be57fef0bf18978593c0fadedb35ab6
isabellaes <borjessonisabella@gmail.com>
2024-04-30 13:51:55 +0200

    implemented function to fetch data by subject

---
+++ app/lib/functions.ts
@@ -0,0 +1,17 @@
+export async function fetchBooksBySubject(query: string): Promise<any> {
+  const apiUrl = `https://openlibrary.org/subjects/${encodeURIComponent(
+    query
+  )}.json`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+"use server";
+
+import { fetchBooksBySubject } from "./lib/functions";
+
+export default async function Home() {
+  const data = await fetchBooksBySubject("romance");
+
   return (
     <main>
       <h1>Start Page</h1>
