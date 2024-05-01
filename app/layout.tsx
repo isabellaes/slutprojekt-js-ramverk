1830e6b5d17bfbaeed411730937d838359fe717a
isabellaes <borjessonisabella@gmail.com>
2024-05-01 19:23:40 +0200

    added styling folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/header/Header";
-import "./global.scss";
+import "./style/global.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
