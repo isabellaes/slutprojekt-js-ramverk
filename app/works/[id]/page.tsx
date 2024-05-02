41e726d78f59ea51f25a84f42d0660c56c5db0e9
isabellaes <borjessonisabella@gmail.com>
2024-05-02 19:16:25 +0200

    add styling to w-page

--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -1,16 +1,16 @@
 import Container from "@/app/components/container/Container";
 import { fetchWorkById } from "@/app/lib/functions";
+import "../workpage.scss";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const data = await fetchWorkById(params.id);
 
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
+    <div className="work-page">
       <Container size="20vw">
         <img
           src={`https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg`}
           alt="cover"
-          style={{ maxHeight: "200px", maxWidth: "100px" }}
         />
         <p>{data.title}</p>
 
