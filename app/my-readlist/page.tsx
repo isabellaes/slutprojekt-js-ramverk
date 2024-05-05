14619c7f158771e9ce3fca0216c98ee75f1c5577
isabellaes <borjessonisabella@gmail.com>
2024-05-05 15:47:14 +0200

    updated readlist

--- app/my-readlist/page.tsx
+++ app/my-readlist/page.tsx
@@ -2,25 +2,20 @@
 
 import { useSelector } from "react-redux";
 import { selectBooks } from "@/app/lib/features/books/bookSlice";
+import Container from "../components/container/Container";
+import List from "../components/list/List";
+import Card from "../components/card/Card";
 
 export default function Page() {
   const books = useSelector(selectBooks);
   return (
-    <>
+    <Container size="100vw">
       <h1>Readlist books</h1>
-      {books.myReadList.map((b) => (
-        <>
-          <p>{b.title}</p>
-          {b.covers ? (
-            <img
-              src={`https://covers.openlibrary.org/b/id/${b.covers[0]}-M.jpg`}
-              alt="cover"
-            />
-          ) : (
-            <img></img>
-          )}
-        </>
-      ))}
-    </>
+      <List direction="row">
+        {books.myReadList.map((b) => (
+          <Card key={b.key} title={b.title} img={b.covers[0]}></Card>
+        ))}
+      </List>
+    </Container>
   );
 }
