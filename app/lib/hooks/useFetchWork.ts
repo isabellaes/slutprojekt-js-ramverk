416e6bb7eaef0abd6f586c1d02a7b0c16daf3062
isabellaes <borjessonisabella@gmail.com>
2024-05-08 11:39:58 +0200

    updated usefetch custom hook to fetch pages

--- app/lib/hooks/useFetchWork.ts
+++ app/lib/hooks/useFetchWork.ts
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { Book } from "@/app/lib/utils/types";
-import { fetchWorkById } from "@/app/lib/utils/api";
+import { fetchWorkById, fetchAverageNumberOfPages } from "@/app/lib/utils/api";
 
 const useFetchWork = (id: string) => {
   const [book, setBook] = useState<Book>();
+  const [pages, setpages] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchWorkById(id);
+      const pages = await fetchAverageNumberOfPages(id);
       if (!ignore) {
+        setpages(pages);
         setBook(data);
       }
     };
@@ -19,7 +22,7 @@ const useFetchWork = (id: string) => {
     };
   }, []);
 
-  return { book };
+  return { book, pages };
 };
 
 export default useFetchWork;
