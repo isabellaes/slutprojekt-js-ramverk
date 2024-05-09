5e980ca3458a6662f3a6ee6399b69d570f513550
isabellaes <borjessonisabella@gmail.com>
2024-05-09 12:05:41 +0200

    added counter and style

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectBooks } from "@/app/lib/features/books/bookSlice";
 import Container from "../components/container/Container";
 import List from "../components/list/List";
-import Card from "../components/card/Card";
 import Button from "../components/button/Button";
 import Modal from "../components/Modal/Modal";
 import Form from "../components/form/Form";
@@ -19,6 +18,7 @@ import {
 import { AppDispatch } from "@/app/lib/features/store";
 import { removeBookFromReadList } from "../lib/features/books/bookSlice";
 import { useState } from "react";
+import Counter from "../components/counter/Counter";
 
 export default function Page() {
   const [selected, setSelected] = useState<string>("");
@@ -31,8 +31,8 @@ export default function Page() {
 
   function calculateAverageRating(): number {
     const total = books.readList.reduce((total, book) => {
-      if (book.comment && book.rating) {
-        return total + Number(book.rating);
+      if (book.revies) {
+        return total + Number(book.revies.rating);
       } else {
         return total;
       }
@@ -45,33 +45,42 @@ export default function Page() {
     <div className="statistics">
       <Container>
         <h1>Statistics</h1>
+        <div className="row space-evenly">
+          <Counter number={books.readList.length} title="Read books"></Counter>
+          <Counter number={calculateTotal(numbers)} title="Pages" />
 
-        <p>Number of books read: {books.readList.length}</p>
-        <p>Total pages: {calculateTotal(numbers)} </p>
-        <p>Number of bookreviews: {booksWithReviews.length}</p>
-        <p>
-          Average number of pages:{" "}
-          {calculateAverage(calculateTotal(numbers), books.readList.length) ||
-            0}
-        </p>
-        <p>Average rating: {calculateAverageRating() || 0}</p>
+          <Counter number={booksWithReviews.length} title="Reviews"></Counter>
+          <Counter
+            number={
+              calculateAverage(
+                calculateTotal(numbers),
+                books.readList.length
+              ) || 0
+            }
+            title="Pages average"
+          ></Counter>
+          <Counter
+            number={calculateAverageRating() || 0}
+            title="Average rating"
+          ></Counter>
+        </div>
 
         <h1>Finished books</h1>
 
         {books.readList.map((b) => (
           <List space="between">
             <div className="row">
-              <img src={b.cover} alt="" />
+              <img src={b.img_url} alt="" />
               <div>
                 <h2>{b.title}</h2>
                 <p>{b.number_of_pages}</p>
               </div>
 
-              {b.comment && b.rating ? (
+              {b.revies?.rating ? (
                 <div className="review">
                   <h2>Review</h2>
-                  <p>Comment: {b.comment}</p>
-                  <p>Rating: {b.rating}/10</p>
+                  <p>Comment: {b.revies.text}</p>
+                  <p>Rating: {b.revies.rating}/10</p>
                 </div>
               ) : (
                 <Button
