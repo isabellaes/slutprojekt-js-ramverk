88c9ace12e5dc74e6e16040b66ad1fb3270f45ac
isabellaes <borjessonisabella@gmail.com>
2024-05-13 11:39:49 +0200

    added sass modules

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,28 +1,28 @@
 "use client";
 
 import Link from "next/link";
-import "./header.scss";
 import Image from "next/image";
 import logo from "./biblioteket.png";
 import Search from "./Search";
+import styles from "./header.module.scss";
 
 const Header = () => {
   return (
-    <header>
-      <Link href={"/"} className="link">
+    <header className={styles.header}>
+      <Link href={"/"} className={styles.link}>
         <Image height={80} src={logo} alt="logotyp"></Image>
       </Link>
 
       <nav>
-        <Link href={"/"} className="link">
+        <Link href={"/"} className={styles.link}>
           <h3>Home</h3>
         </Link>
         <h2>|</h2>
-        <Link href={"/favourite"} className="link">
+        <Link href={"/favourite"} className={styles.link}>
           <h3>My favourites</h3>
         </Link>
         <h2>|</h2>
-        <Link href={"/statistics"} className="link">
+        <Link href={"/statistics"} className={styles.link}>
           <h3>Statistics</h3>
         </Link>
       </nav>
