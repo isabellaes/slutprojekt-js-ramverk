4c48a785451c70b8f8bb2ab19c48bda927e44d39
isabellaes <borjessonisabella@gmail.com>
2024-05-15 19:44:00 +0200

    removde consolelog

--- app/search/author/page.tsx
+++ app/search/author/page.tsx
@@ -25,8 +25,6 @@ export default async function Page({
     offset
   );
 
-  console.log(offset);
-
   return (
     <Container>
       <h1 className={style.title}>Search results for "{searchParams.query}"</h1>
